8580c6d435b69e4d7c66788676a535194e37497f
alx <37true@gmail.com>
2016-11-10 19:48:14 +0700

    final project with homework

--- app/src/main/java/tk/alltrue/geoquiz/CheatActivity.java
+++ app/src/main/java/tk/alltrue/geoquiz/CheatActivity.java
@@ -1,10 +1,18 @@
 package tk.alltrue.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+    public static final String EXTRA_ANSWER_IS_TRUE="tk.alltrue.geoquiz.answer_is_true";
 
+    public static Intent newIntent(Context packageContext, boolean answerTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerTrue);
+        return i;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
