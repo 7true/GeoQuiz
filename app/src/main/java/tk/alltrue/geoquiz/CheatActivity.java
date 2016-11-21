fe18e5d2a6d5e88b4dcef8a14a5a4bae421bd5be
alx <37true@gmail.com>
2016-11-22 01:49:53 +0700

    final project with homework

--- app/src/main/java/tk/alltrue/geoquiz/CheatActivity.java
+++ app/src/main/java/tk/alltrue/geoquiz/CheatActivity.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
+    private static final String TAG = "CheatActivity";
     public static final String EXTRA_ANSWER_IS_TRUE="tk.alltrue.geoquiz.answer_is_true";
     private boolean mAnswerIsTrue;
+    private boolean mCheat = false;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private static final String KEY_CHEAT = "cheat";
     public static final String EXTRA_ANSWER_SHOWN =
             "tk.alltrue.geoquiz.answer_shown";
 
@@ -24,6 +28,7 @@ public class CheatActivity extends AppCompatActivity {
     public static boolean wasAnswerShown(Intent result) {
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +38,10 @@ public class CheatActivity extends AppCompatActivity {
         mAnswerTextView = (TextView)findViewById(R.id.answer_text_view);
 
         mShowAnswer = (Button)findViewById(R.id.show_answer_button);
+        if (savedInstanceState != null) {
+            mCheat = savedInstanceState.getBoolean(KEY_CHEAT);
+            setAnswerShownResult(mCheat);
+        }
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +51,7 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+                mCheat = true;
             }
         });
     }
@@ -49,5 +59,12 @@ public class CheatActivity extends AppCompatActivity {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK , data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(KEY_CHEAT, mCheat);
     }
 }
